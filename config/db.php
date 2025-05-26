47468825247ac50c4b593337e1c15565120c8083
Dhanesh-Trivedi <dhaneshtrivedi.20.cs@iite.indusuni.ac.in>
2025-05-26 16:45:37 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -6,7 +6,7 @@ $password = "aFYqEPKSyNOHSrdnNNEyCMdjWxmxUKyt";
 $dbname = "railway";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("DB Connection failed: " . $e->getMessage());
